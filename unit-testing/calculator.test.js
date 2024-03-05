1afc91eea18cb3d46b54b939fe7a9bb66a8b57e0
techstarteranhkim <anh.kim@docc.techstarter.de>
2024-03-05 13:56:16 +0100

    Describe, arrange+act+assert

--- unit-testing/calculator.test.js
+++ unit-testing/calculator.test.js
@@ -1,71 +1,111 @@
 const calculator = require("./calculator");
+describe("Unterrichtsbeispiele", () => {
+  test("Die Summe von 2 + 5 ist 7", () => {
+    //arrange+act
+    const result = calculator.sum(2, 5);
+    //assert
+    expect(result).toEqual(7);
+  });
 
-test("Die Summe von 2 + 5 ist 7", () => {
-  const result = calculator.sum(2, 5);
+  test('Die Summe von "Apfel" + 5 ist "Apfel5"', () => {
+    //arrange+act
+    const result = calculator.sum("Apfel", 5);
+    //assert
+    expect(result).toEqual("Apfel5");
+  });
 
-  expect(result).toEqual(7);
+  test("Der Quotient aus 10 durch 2 ist 5", () => {
+    //arrange+act
+    const result = calculator.divide(10, 2);
+    //assert
+    expect(result).toEqual(5);
+  });
+
+  test("Der Quotient aus 10 durch 3 ist nahe an 3,33", () => {
+    //arrange+act
+    const result = calculator.divide(10, 3);
+    //assert
+    expect(result).toBeCloseTo(3.33, 2);
+  });
+
+  test("Postive durch negative Zahl ist kleiner als 0", () => {
+    //arrange+act
+    const result = calculator.divide(10, -2);
+    //assert
+    expect(result).toBeLessThan(0);
+  });
+
+  test("Negativ durch negative Zahl ist größer als 0", () => {
+    //arrange+act
+    const result = calculator.divide(-10, -2);
+    //assert
+    expect(result).toBeGreaterThan(0);
+  });
+});
+describe("Testen von Subtraktion ", () => {
+  test("Subtraktion zweier positiver Zahlen", () => {
+    //arrange
+    const a = 10;
+    const b = 20;
+    const expected = -10;
+    //act
+    const result = calculator.subtract(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
+
+  test("Subtraktion zweier negativer Zahlen", () => {
+    //arrange
+    const a = -10;
+    const b = -20;
+    const expected = 10;
+    //act
+    const result = calculator.subtract(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
+  test("Subtraktion von 0 und einer negativen Zahl", () => {
+    //arrange
+    const a = 0;
+    const b = -10;
+    const expected = 10;
+    //act
+    const result = calculator.subtract(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
 });
 
-test('Die Summe von "Apfel" + 5 ist "Apfel5"', () => {
-  const result = calculator.sum("Apfel", 5);
+describe("Testen von Multiplikation ", () => {
+  test("Multiplikation zweier positiver Zahlen", () => {
+    //arrange
+    const a = 10;
+    const b = 20;
+    const expected = 200;
+    //act
+    const result = calculator.multiply(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
 
-  expect(result).toEqual("Apfel5");
-});
-
-test("Der Quotient aus 10 durch 2 ist 5", () => {
-  const result = calculator.divide(10, 2);
-
-  expect(result).toEqual(5);
-});
-
-test("Der Quotient aus 10 durch 3 ist nahe an 3,33", () => {
-  const result = calculator.divide(10, 3);
-
-  expect(result).toBeCloseTo(3.33, 2);
-});
-
-test("Postive durch negative Zahl ist kleiner als 0", () => {
-  const result = calculator.divide(10, -2);
-
-  expect(result).toBeLessThan(0);
-});
-
-test("Negativ durch negative Zahl ist größer als 0", () => {
-  const result = calculator.divide(-10, -2);
-
-  expect(result).toBeGreaterThan(0);
-});
-
-test("Subtraktion zweier positiver Zahlen", () => {
-  const result = calculator.subtract(10, 20);
-
-  expect(result).toBe(-10);
-});
-
-test("Subtraktion zweier negativer Zahlen", () => {
-  const result = calculator.subtract(-10, -20);
-
-  expect(result).toBe(10);
-});
-test("Subtraktion von 0 und einer negativen Zahl", () => {
-  const result = calculator.subtract(0, -10);
-
-  expect(result).toBe(10);
-});
-
-test("Multiplikation zweier positiver Zahlen", () => {
-  const result = calculator.multiply(10, 20);
-
-  expect(result).toBe(200);
-});
-
-test("Multiplikation zweier negativer Zahlen", () => {
-  const result = calculator.multiply(-10, -20);
-
-  expect(result).toBe(200);
-});
-test("Multiplikation mit einer 0", () => {
-  const result = calculator.multiply(-10, 0);
-
-  expect(result).toBe(0);
+  test("Multiplikation zweier negativer Zahlen", () => {
+    //arrange
+    const a = -10;
+    const b = -20;
+    const expected = 200;
+    //act
+    const result = calculator.multiply(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
+  test("Multiplikation mit einer 0", () => {
+    //arrange
+    const a = 10;
+    const b = 0;
+    const expected = 0;
+    //act
+    const result = calculator.multiply(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
 });
--- unit-testing/minMax.test.js
+++ unit-testing/minMax.test.js
@@ -1,25 +1,71 @@
 const minMax = require("./minMax");
 
-test("Minimum zweiter Zahlen mit a>b", () => {
-  expect(minMax.min(5, 3)).toBe(3);
+describe("minimum", () => {
+  test("Minimum zweiter Zahlen mit a>b", () => {
+    //arrange
+    const a = 5;
+    const b = 3;
+    const expected = b;
+    //act
+    result = minMax.min(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
+
+  test("Minimum zweiter Zahlen mit a<b", () => {
+    //arrange
+    const a = 3;
+    const b = 6;
+    const expected = a;
+    //act
+    result = minMax.min(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
+
+  test("Minimum zweiter Zahlen mit a=b", () => {
+    //arrange
+    const a = 5;
+    const b = 5;
+    const expected = a;
+    //act
+    result = minMax.min(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
 });
 
-test("Minimum zweiter Zahlen mit a<b", () => {
-  expect(minMax.min(3, 6)).toBe(3);
-});
+describe("maximum", () => {
+  test("Maximum zweiter Zahlen mit a>b", () => {
+    //arrange
+    const a = 5;
+    const b = 3;
+    const expected = a;
+    //act
+    result = minMax.max(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
 
-test("Minimum zweiter Zahlen mit a=b", () => {
-  expect(minMax.min(5, 5)).toBe(5);
-});
+  test("Maximum zweiter Zahlen mit a<b", () => {
+    //arrange
+    const a = 3;
+    const b = 6;
+    const expected = b;
+    //act
+    result = minMax.max(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
 
-test("Maximum zweiter Zahlen mit a>b", () => {
-  expect(minMax.max(5, 3)).toBe(5);
-});
-
-test("Maximum zweiter Zahlen mit a<b", () => {
-  expect(minMax.max(3, 6)).toBe(6);
-});
-
-test("Maximum zweiter Zahlen mit a=b", () => {
-  expect(minMax.max(5, 5)).toBe(5);
+  test("Maximum zweiter Zahlen mit a=b", () => {
+    //arrange
+    const a = 5;
+    const b = 5;
+    const expected = a;
+    //act
+    result = minMax.max(a, b);
+    //assert
+    expect(result).toBe(expected);
+  });
 });
